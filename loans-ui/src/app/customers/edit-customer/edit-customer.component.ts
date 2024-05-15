3fd796474bd276449482cf25a50d9b604586d356
udaymunasu <udaymunasu311999@gmail.com>
2024-05-15 13:27:33 +0530

    new customer addition changes

--- loans-ui/src/app/customers/edit-customer/edit-customer.component.ts
+++ loans-ui/src/app/customers/edit-customer/edit-customer.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-edit-customer',
   templateUrl: './edit-customer.component.html',
-  styleUrls: ['./edit-customer.component.scss']
+  styleUrls: ['./edit-customer.component.scss'],
 })
 export class EditCustomerComponent implements OnInit {
+  customerDetails: Object;
+  customer: any;
 
-  constructor() { }
-
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private customersService: CustomerService
+  ) {}
+  customerId;
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data: any) => {
+      this.customerId = data.id;
+    });
+    if (this.customerId) {
+      // Fetch customer details
+      this.customersService.viewCustomer(this.customerId).subscribe(
+        (data) => {
+          // Check if data is received successfully
+          if (data && data['user']) {
+            this.customerDetails = data;
+            this.customer = this.customerDetails['user'];
+            console.log('Edit the customer', this.customer);
+          } else {
+            console.error('Customer data not found');
+          }
+        },
+        (error) => {
+          console.error('Error fetching customer details:', error);
+        }
+      );
+    } else {
+      console.error('Customer ID is undefined or null');
+    }
   }
-
 }
